0ab7955faf688a0bc985d7d7ef315e7fb7cc7dea
KSHyun9692 <KSHyun9692@gmail.com>
2021-11-22 20:29:13 +0900

    reactStudy

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import Button from "./Button";
 import styles from "./App.module.css"
 import { useState, useEffect } from "react";
 
+function Hello(){
+  useEffect(() => {
+    console.log("created :)");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
+  const [showing, setShowing] = useState(false);
+  //const onClick = () => setValue((prev) => prev + 1);
+  const onClick = () => setShowing((prev) => !prev);
   const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
 
@@ -33,7 +42,9 @@ function App() {
       <Button text={"Continue"} />
 
       <h1>{counter}</h1>
-      <button onClick={onClick}>click me!!</button>
+      {/* <button onClick={onClick}>click me!!</button> */}
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
